cfb152c0636a3e08db0e2f4d75e8f81b7167cbea
Eshan Das <eshandasnit@gmail.com>
2018-02-20 14:11:16 +0530

    Removed django plugin

--- rule_engine/rules/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from rule_engine.rules.models import Rule, TriggeredRules, ExecutionSummary
-
-admin.site.register(Rule)
-admin.site.register(TriggeredRules)
-admin.site.register(ExecutionSummary)
--- rule_engine/rules/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppauthConfig(AppConfig):
-    name = 'appauth'
-    verbose_name = 'App Auth'
