c8a81312c9123fdcc31b013988e6ae586aabf3fb
Eshan Das <eshandasnit@gmail.com>
2018-02-22 15:52:35 +0530

    Bumped up version number

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     name='pyrete',
     packages=find_packages(),
     include_package_data=True,
-    version='0.1.0',
+    version='0.2.0',
     description='Rete algorithm based Rule Engine built on Python',
     author='Eshan Das',
     author_email='eshandasnit@gmail.com',
     url='https://github.com/eshandas/pyrete',  # use the URL to the github repo
-    download_url='https://github.com/eshandas/pyrete/archive/0.1.0.tar.gz',  # Create a tag in github
+    download_url='https://github.com/eshandas/pyrete/archive/0.2.0.tar.gz',  # Create a tag in github
     keywords=['rete', 'net', 'network', 'rule', 'engine'],
     classifiers=[],
 )
